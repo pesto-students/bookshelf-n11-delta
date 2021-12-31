4f646f02f3817f582c7d669dda7c2fda0ecbf91f
Shubham Bansal <svb@Shubhams-MacBook-Air.local>
2021-12-31 16:26:31 +0530

    Added pagination to orders page

--- src/components/Orders/Orders.tsx
+++ src/components/Orders/Orders.tsx
@@ -10,25 +10,28 @@ import {
   TableHead,
   TableRow,
   Typography,
+  Pagination,
 } from "@mui/material";
 import moment from "moment";
-import {useContext, useEffect, useState} from "react";
-import {useNavigate} from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-import {AppContext} from "../../App/App";
+import { AppContext } from "../../App/App";
 import axios from "../../core/axios";
 import environment from "../../Environment/environment";
-import {Overlay} from "../../shared/components";
-import {DASHBOARD_ROUTE} from "../../shared/immutables";
+import { Overlay } from "../../shared/components";
+import { DASHBOARD_ROUTE } from "../../shared/immutables";
 import styles from "./Orders.module.scss";
 
 export const Orders = () => {
   const [orders, setOrders] = useState([]);
   const [isLoading, setLoading] = useState(false);
+  const [page, setPage] = useState(1);
+  const rowsPerPage = 5;
 
   const navigate = useNavigate();
   const {
-    appState: {books, isUserLoggedIn},
+    appState: { books, isUserLoggedIn },
   } = useContext(AppContext);
 
   useEffect(() => {
@@ -57,7 +60,7 @@ export const Orders = () => {
     setLoading(true);
     axios
       .get(`${environment.API_URL}/orders`)
-      .then(({data}) => {
+      .then(({ data }) => {
         setOrders(data.orders);
       })
       .catch((error) => {
@@ -74,101 +77,133 @@ export const Orders = () => {
       ) : (
         <Paper elevation={2} className={styles.layout}>
           <Stack direction="column">
-            <Typography style={{fontWeight: "bold"}} variant="h5">
-              Orders
-            </Typography>
+            <Stack
+              direction="row"
+              justifyContent="space-between"
+              alignItems="center"
+            >
+              <Typography style={{ fontWeight: "bold" }} variant="h5">
+                Orders
+              </Typography>
+              {!!orders.length ? (
+                <Pagination
+                  count={
+                    orders.length % rowsPerPage === 0
+                      ? orders.length / rowsPerPage
+                      : Math.ceil(orders.length / rowsPerPage)
+                  }
+                  page={page}
+                  onChange={(event, val) => setPage(val)}
+                  variant="outlined"
+                  shape="rounded"
+                />
+              ) : (
+                <></>
+              )}
+            </Stack>
             {!!orders.length ? (
-              orders.map((order) => (
-                <Grid container rowSpacing={0} className={styles.orderOuterBox}>
-                  <Grid item xs={8}>
-                    <Paper elevation={2}>
-                      <TableContainer>
-                        <Table
-                          size="small"
-                          sx={{
-                            tableLayout: "fixed",
-                          }}
-                        >
-                          <TableHead style={{backgroundColor: "#8e99d7"}}>
-                            <TableRow>
-                              <TableCell>#</TableCell>
-                              <TableCell align="right">Book Name</TableCell>
-                              <TableCell align="right">Price</TableCell>
-                              <TableCell align="right">Quantity</TableCell>
-                            </TableRow>
-                          </TableHead>
-                          <TableBody>
-                            {order.orderDetails.map((orderDetail, index) => (
-                              <TableRow
-                                key={orderDetail?.bookId?._id}
-                                style={
-                                  index % 2
-                                    ? {background: "#EDEDEF"}
-                                    : {background: "white"}
-                                }
-                              >
-                                <TableCell>{index + 1}</TableCell>
-                                <TableCell
-                                  align="right"
-                                  style={{cursor: "pointer"}}
-                                  onClick={() =>
-                                    redirectToBooksPage(
-                                      orderDetail?.bookId?._id
-                                    )
+              orders
+                .slice(
+                  (page - 1) * rowsPerPage,
+                  (page - 1) * rowsPerPage + rowsPerPage
+                )
+                .map((order) => (
+                  <Grid
+                    container
+                    rowSpacing={0}
+                    className={styles.orderOuterBox}
+                  >
+                    <Grid item xs={8}>
+                      <Paper elevation={2}>
+                        <TableContainer>
+                          <Table
+                            size="small"
+                            sx={{
+                              tableLayout: "fixed",
+                            }}
+                          >
+                            <TableHead style={{ backgroundColor: "#8e99d7" }}>
+                              <TableRow>
+                                <TableCell>#</TableCell>
+                                <TableCell align="right">Book Name</TableCell>
+                                <TableCell align="right">Price</TableCell>
+                                <TableCell align="right">Quantity</TableCell>
+                              </TableRow>
+                            </TableHead>
+                            <TableBody>
+                              {order.orderDetails.map((orderDetail, index) => (
+                                <TableRow
+                                  key={orderDetail?.bookId?._id}
+                                  style={
+                                    index % 2
+                                      ? { background: "#EDEDEF" }
+                                      : { background: "white" }
                                   }
                                 >
-                                  {orderDetail?.bookId?.title}
-                                </TableCell>
-                                <TableCell align="right">
-                                  {orderDetail.price}
-                                </TableCell>
-                                <TableCell align="right">
-                                  {orderDetail.quantity}
+                                  <TableCell>{index + 1}</TableCell>
+                                  <TableCell
+                                    align="right"
+                                    style={{ cursor: "pointer" }}
+                                    onClick={() =>
+                                      redirectToBooksPage(
+                                        orderDetail?.bookId?._id
+                                      )
+                                    }
+                                  >
+                                    {orderDetail?.bookId?.title}
+                                  </TableCell>
+                                  <TableCell align="right">
+                                    {orderDetail.price}
+                                  </TableCell>
+                                  <TableCell align="right">
+                                    {orderDetail.quantity}
+                                  </TableCell>
+                                </TableRow>
+                              ))}
+                            </TableBody>
+                          </Table>
+                        </TableContainer>
+                      </Paper>
+                    </Grid>
+                    <Grid item xs={4}>
+                      <Paper elevation={0} className={styles.orderInnerBox}>
+                        <TableContainer>
+                          <Table
+                            size="small"
+                            sx={{
+                              tableLayout: "fixed",
+                            }}
+                          >
+                            <TableBody>
+                              <TableRow>
+                                <TableCell>Order Value</TableCell>
+                                <TableCell>{order.value}</TableCell>
+                              </TableRow>
+                              <TableRow>
+                                <TableCell>Order Date</TableCell>
+                                <TableCell>
+                                  {moment(order.createdOn).format("DD-MM-YYYY")}
                                 </TableCell>
                               </TableRow>
-                            ))}
-                          </TableBody>
-                        </Table>
-                      </TableContainer>
-                    </Paper>
+                              <TableRow>
+                                <TableCell>Delivery Date</TableCell>
+                                <TableCell>
+                                  {moment(order.deliveredOn).format(
+                                    "DD-MM-YYYY"
+                                  )}
+                                </TableCell>
+                              </TableRow>
+                              <TableRow>
+                                <TableCell>Payment Method</TableCell>
+                                <TableCell>{order.paymentMethod}</TableCell>
+                              </TableRow>
+                            </TableBody>
+                          </Table>
+                        </TableContainer>
+                      </Paper>
+                    </Grid>
                   </Grid>
-                  <Grid item xs={4}>
-                    <Paper elevation={0} className={styles.orderInnerBox}>
-                      <TableContainer>
-                        <Table
-                          size="small"
-                          sx={{
-                            tableLayout: "fixed",
-                          }}
-                        >
-                          <TableBody>
-                            <TableRow>
-                              <TableCell>Order Value</TableCell>
-                              <TableCell>{order.value}</TableCell>
-                            </TableRow>
-                            <TableRow>
-                              <TableCell>Order Date</TableCell>
-                              <TableCell>
-                                {moment(order.createdOn).format("DD-MM-YYYY")}
-                              </TableCell>
-                            </TableRow>
-                            <TableRow>
-                              <TableCell>Delivery Date</TableCell>
-                              <TableCell>
-                                {moment(order.deliveredOn).format("DD-MM-YYYY")}
-                              </TableCell>
-                            </TableRow>
-                            <TableRow>
-                              <TableCell>Payment Method</TableCell>
-                              <TableCell>{order.paymentMethod}</TableCell>
-                            </TableRow>
-                          </TableBody>
-                        </Table>
-                      </TableContainer>
-                    </Paper>
-                  </Grid>
-                </Grid>
-              ))
+                ))
             ) : (
               <Grid
                 container
