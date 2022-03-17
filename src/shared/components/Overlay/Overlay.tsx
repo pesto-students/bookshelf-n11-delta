f52bf57fb60d545cd6519af227b9db3c3a3d2510
Alisha <alisha.mahajan265@gmail.com>
2022-03-17 17:28:28 +0530

    chore(routing): fix netlify routing issue

--- src/shared/components/Overlay/Overlay.tsx
+++ src/shared/components/Overlay/Overlay.tsx
@@ -4,13 +4,13 @@ import styles from './Overlay.module.scss';
 
 export function Overlay({showBackdrop = false}) {
   const spinner = (
-    <CircularProgress className={styles.overlay} color="inherit" />
+    <CircularProgress className={styles.overlay}/>
   );
 
   if (!!showBackdrop) {
     return (
       <Backdrop
-        sx={{color: '#3f51b5', zIndex: theme => theme.zIndex.drawer + 1}}
+        sx={{zIndex: theme => theme.zIndex.drawer + 1}}
         open={true}
       >
         {spinner}
